b629a266ffb9aedd65dfe3db8a3cb94a826b7503
Helia Gohar <helia77@users.noreply.github.com>
2023-03-22 17:56:11 -0500

    Plane transition in volume range
    Global variable update added

--- glOrthoView.cpp
+++ glOrthoView.cpp
@@ -18,12 +18,12 @@ const char* glsl_version = "#version 130";              // specify the version o
 ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);   // specify the OpenGL color used to clear the back buffer
 float gui_VolumeSize[] = { 1.0f, 1.0f, 1.0f };            // initialize the volume size to 1 (uniform)
 float gui_VolumeSlice[] = { 0.5f, 0.5f, 0.5f };         // current volume slice being displayed [0.0, 1.0]
-float coordinates[] = { 0.0f, 0.0f, 0.0f };
-
+float coords[] = { 0.0f, 0.0f, 0.0f };
+bool window_focused = false;
 tira::camera cam;                                       // create a perspective camera for 3D visualization of the volume
 bool right_mouse_pressed = false;                       // flag indicates when the right mouse button is being dragged
 bool left_mouse_pressed = false;                        // flag indicates when the left mouse button is being dragged
-bool ctrl = false;
+
 double mouse_x, mouse_y;
 double THETA = 0.02;
 
@@ -41,14 +41,6 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
     }
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
         left_mouse_pressed = false;
-}
-static void crtl_mouse_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_LEFT_CONTROL && action == GLFW_PRESS) {
-        std::cout << "Select Coordinate.\n";
-        ctrl = true;
-    }
-    /*if (key == GLFW_KEY_LEFT_CONTROL && action == GLFW_RELEASE)
-        ctrl = false;*/
 }
         
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
@@ -220,15 +212,39 @@ glm::mat4 createTransMatrix(int horz_axis, int vert_axis, glm::vec3 volume_size,
     return Translation_matrix;
 }
 
-void MaxRange(float &num, float MaxRange) {
-    int sign;
-    if (abs(num) > MaxRange) {
-        sign = (num >= 0) ? 1 : -1;
-        num = sign * MaxRange;
-    }
+void SetGlobalVariables(glm::vec3 plane_position, glm::vec3 coordinates) {
+    gui_VolumeSlice[0] = plane_position.x;
+    gui_VolumeSlice[1] = plane_position.y;
+    gui_VolumeSlice[2] = plane_position.z;
+
+    coords[0] = coordinates.x;
+    coords[1] = coordinates.y;
+    coords[2] = coordinates.z;
 }
 
-void coordinates_select(GLFWwindow* window, int display_w, int display_h, glm::vec3 volume_size, glm::vec3 &plane_position) {
+void MaxRange(glm::vec3 &num, glm::vec3 MaxRange) {
+    int sign;
+    if (abs(num.x) > MaxRange.x) {
+        sign = (num.x >= 0) ? 1 : -1;
+        num.x = sign * MaxRange.x;
+    }
+    if (abs(num.y) > MaxRange.y) {
+        sign = (num.y >= 0) ? 1 : -1;
+        num.y = sign * MaxRange.y;
+    }
+    if (abs(num.z) > MaxRange.z) {
+        sign = (num.z >= 0) ? 1 : -1;
+        num.z = sign * MaxRange.z;
+    }
+
+}
+bool IsInRange(glm::vec3 num, glm::vec3 MaxRange) {
+    if (abs(num.x) < MaxRange.x && abs(num.y) < MaxRange.y && abs(num.z) < MaxRange.z)
+        return true;
+    return false;
+}
+
+void coordinates_select(GLFWwindow* window, glm::vec3 &coordinates, int display_w, int display_h, glm::vec3 volume_size, glm::vec3 &plane_position) {
     
     double coord_x, coord_y;
     glfwGetCursorPos(window, &coord_x, &coord_y);
@@ -236,62 +252,57 @@ void coordinates_select(GLFWwindow* window, int display_w, int display_h, glm::v
     glm::vec2 ortho_world = VolSizeMax(aspect, volume_size);
     int half_disp_w = display_w / 2;
     int half_disp_h = display_h / 2;
-    
-     
+    bool InRange = false;
+
+
     // XY plane
     // window x: (800, 1600)   y: (0, 600)
+    // maps all coordinates to the VolumeSize * aspect ratio
     if (coord_x > half_disp_w && coord_y < half_disp_h && left_mouse_pressed) {
-        coordinates[0] = ((coord_x / half_disp_w) - 1) * ortho_world.x - (ortho_world.x / 2.0f);
-        MaxRange(coordinates[0], volume_size.x / 2.0f);
-
-        coordinates[1] = -((coord_y / half_disp_h) * ortho_world.y - (ortho_world.y / 2.0f));
-        MaxRange(coordinates[1], volume_size.y / 2.0f);
-
-        coordinates[2] = plane_position.z - 0.5f;
+        coordinates.x = ((coord_x / half_disp_w) - 1) * ortho_world.x - (ortho_world.x / 2.0f);
+        coordinates.y = -((coord_y / half_disp_h) * ortho_world.y - (ortho_world.y / 2.0f));
+        coordinates.z = plane_position.z - 0.5f;
     }
     // XZ plane
     // window x: (800, 1600)   y: (600, 1200)
+    // maps all coordinates to the VolumeSize * aspect ratio
     if (coord_x > half_disp_w && coord_y > half_disp_h && left_mouse_pressed) {
-        coordinates[0] = ((coord_x / half_disp_w) - 1) * ortho_world.x - (ortho_world.x / 2.0f);
-        MaxRange(coordinates[0], volume_size.x / 2.0f);
-
-        coordinates[1] = plane_position.y - 0.5f;
-
-        coordinates[2] = -(((coord_y / half_disp_h) - 1) * ortho_world.y - (ortho_world.y/ 2.0f));
-        MaxRange(coordinates[2], volume_size.z / 2.0f);
+        coordinates.x = ((coord_x / half_disp_w) - 1) * ortho_world.x - (ortho_world.x / 2.0f);
+        coordinates.y = plane_position.y - 0.5f;
+        coordinates.z = -(((coord_y / half_disp_h) - 1) * ortho_world.y - (ortho_world.y/ 2.0f));
     }
 
     // YZ plane
     // window x: (800, 1600)   y: (0, 600)
+    // maps all coordinates to the VolumeSize * aspect ratio
     if (coord_x < half_disp_w && coord_y > half_disp_h && left_mouse_pressed) {
-        coordinates[0] = plane_position.x - 0.5f;
-
-        coordinates[1] = (coord_x / half_disp_w) * ortho_world.x - (ortho_world.x / 2.0f);
-        MaxRange(coordinates[1], volume_size.y / 2.0f);
-
-        coordinates[2] = -(((coord_y / half_disp_h) - 1) * ortho_world.y - (ortho_world.y / 2.0f));
-        MaxRange(coordinates[2], volume_size.z / 2.0f);
+        coordinates.x = plane_position.x - 0.5f;
+        coordinates.y = (coord_x / half_disp_w) * ortho_world.x - (ortho_world.x / 2.0f);
+        coordinates.z = -(((coord_y / half_disp_h) - 1) * ortho_world.y - (ortho_world.y / 2.0f));
     }
 
     // maps back to (0,1)
-    if (ctrl) {
-        plane_position.x = coordinates[0] / volume_size.x + 0.5f;
-        plane_position.y = coordinates[1] / volume_size.y + 0.5f;
-        plane_position.z = coordinates[2] / volume_size.z + 0.5f;
+    MaxRange(coordinates, volume_size / 2.0f);                          // if any of the selected coordinates are outside the volume, sets it the nearest value
+    InRange = IsInRange(coordinates, volume_size / 2.0f);               // checks if all the selected coordinates are in the volume range
+
+    if(InRange)                                                         // changes the plane position only when user clicks on the volume area
+    {
+        plane_position.x = coordinates.x / volume_size.x + 0.5f;
+        plane_position.y = coordinates.y / volume_size.y + 0.5f;
+        plane_position.z = coordinates.z / volume_size.z + 0.5f;
     }
-    
+
 }
 
 void resetPlane(float vs_max) {
     for (int i = 0; i < 3; i++) {
         gui_VolumeSize[i] = 1.0f;
         gui_VolumeSlice[i] = 0.5f;
-        //coordinates[i] = 0.0f;
+        coords[i] = 0.0f;
     }
     cam.setPosition(2 * vs_max, 2 * vs_max, 2 * vs_max);
     cam.LookAt(0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 }
-
 
 /// <summary>
 /// Renders all three planes using different viewports 
@@ -357,13 +368,13 @@ int main(int argc, char** argv)
 {
     // Initialize OpenGL
     window = InitGLFW();                                // create a GLFW window
-    glfwSetKeyCallback(window, crtl_mouse_callback);
+
     glfwSetMouseButtonCallback(window, mouse_button_callback);
     glfwSetCursorPosCallback(window, cursor_position_callback);
-    
-    
+
     InitUI(window, glsl_version);                       // initialize ImGui
 
+    
     GLenum err = glewInit();
     if (GLEW_OK != err) {
         /* Problem: glewInit failed, something is seriously wrong. */
@@ -372,8 +383,8 @@ int main(int argc, char** argv)
 
     // Load or create an example volume
     tira::glVolume<unsigned char> vol;
-    //vol.load("data/*.bmp");                                                         // uncomment to load from the demo image stack
-    vol.generate_rgb(256, 256, 256);                                           // generate an RGB grid texture
+    vol.load("data/*.bmp");                                                    // uncomment to load from the demo image stack
+    //vol.generate_rgb(256, 256, 256);                                           // generate an RGB grid texture
 
 
     // generate the basic geometry and materials for rendering
@@ -407,7 +418,9 @@ int main(int argc, char** argv)
 
         glm::vec3 volume_size = glm::vec3(gui_VolumeSize[0], gui_VolumeSize[1], gui_VolumeSize[2]);
         glm::vec3 plane_position = glm::vec3(gui_VolumeSlice[0], gui_VolumeSlice[1], gui_VolumeSlice[2]);
+        glm::vec3 coordinates = glm::vec3(coords[0], coords[1], coords[2]);
         
+
         // Projection Matrix
         float aspect = (float)display_w / (float)display_h;
         glm::mat4 Mproj = createProjectionMatrix(aspect, volume_size);
@@ -423,9 +436,16 @@ int main(int argc, char** argv)
         /****************************************************/
 
         glm::mat4 ViewMatrix(1.0f);
-        coordinates_select(window, display_w, display_h, volume_size, plane_position);
-         
-        
+
+        // coordination selection is not applied when user clicks on the imgui window
+        if (!window_focused)                                      
+            coordinates_select(window, coordinates, display_w, display_h, volume_size, plane_position);
+
+
+        // Sets global varilabes (gui_VolumeSlice and coords) to the updated values and view on imgui window
+        SetGlobalVariables(plane_position, coordinates);
+
+
         // Bind the volume material and render all of the viewports
         
         // render - Upper Right (X-Y) Viewport
