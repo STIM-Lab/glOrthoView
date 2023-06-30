218774fad9b185e04e2554fcf5a8ce6c790cf3a3
David Mayerich <mayerich@uh.edu>
2023-06-30 09:19:55 -0500

    pseudocode for script to create demo data

---
+++ data/stack2npy.py
@@ -0,0 +1,9 @@
+import numpy as np
+import skimage as ski
+
+# load the images in the /stack directory to a NumPy array
+
+# output that NumPy array in two different formats: unsigned char, float
+# make sure to name them two different things (stack_float.npy, stack_uchar.npy)
+
+# add those files to the repository and push
